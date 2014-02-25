558494d0e5754f55d48137c8ce4232dc487ef4e0
marcelomarshmallow <marcel.kolzau@gmx.de>
2014-02-25 10:22:15 +0100

    Create Zuruecksetzen.java

---
+++ Zuruecksetzen.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Zuruecksetzen extends Frame
+{  
+  //----------------------------------------------------------------------------
+  // Schriftarten definieren
+  //----------------------------------------------------------------------------
+  Font Ueberschrift = new Font("Arial", Font.BOLD, 16);
+  Font Normal = new Font("Arial", Font.PLAIN, 12);
+  //----------------------------------------------------------------------------
+  
+  public Zuruecksetzen()
+  {
+    double U = 0;
+    double I = 0;
+    double R = 0;
+    double C = 0;
+    
+    
+    System.out.println("Spannung = " +U+ " V");
+    
+    setVisible(false);
+    
+  }
+}
